d863aeb16b579ef432ef89dc0911a9306d6018af
Jean Connory <michaelrhcp@gmail.com>
2023-07-18 16:39:13 -0300

    Camada de application com interfaces, dtos e automapper

---
+++ CleanArchMvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+	public interface ICategoryService
+	{
+		Task<IEnumerable<CategoryDTO>> GetCategories();
+		Task<CategoryDTO> GetById(int? id);
+		Task Add(CategoryDTO categoryDto);
+		Task Update(CategoryDTO categoryDto);
+		Task Remove(int? id);
+	}
+}
