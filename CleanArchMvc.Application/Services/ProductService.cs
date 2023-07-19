15b91bb95ddff6f618287f1d39981e1219d0a462
Jean Connory <michaelrhcp@gmail.com>
2023-07-19 20:07:48 -0300

    retirando metodo duplicado

--- CleanArchMvc.Application/Services/ProductService.cs
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -45,17 +45,17 @@ namespace CleanArchMvc.Application.Services
 			return _mapper.Map<ProductDTO>(result);
 		}
 
-		public async Task<ProductDTO> GetProductByCategory(int? id)
-		{
-			var productByIdQuery = new GetProductByIdQuery(id.Value);
+		//public async Task<ProductDTO> GetProductByCategory(int? id)
+		//{
+		//	var productByIdQuery = new GetProductByIdQuery(id.Value);
 
-			if (productByIdQuery == null)
-				throw new Exception($"Entity could not be loaded");
+		//	if (productByIdQuery == null)
+		//		throw new Exception($"Entity could not be loaded");
 
-			var result = await _mediator.Send(productByIdQuery);
+		//	var result = await _mediator.Send(productByIdQuery);
 
-			return _mapper.Map<ProductDTO>(result);
-		}
+		//	return _mapper.Map<ProductDTO>(result);
+		//}
 
 		public async Task Add(ProductDTO productDto)
 		{
